58af02c42824e6bc60f67aca91384d63be56e820
mvrc <phaidos@gmail.com>
2024-05-10 09:25:42 +0300

    Added "About" action to system tray menu

--- ddcbright-1.0/usr/bin/ddcbright/tray.py
+++ ddcbright-1.0/usr/bin/ddcbright/tray.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QMessageBox
 from PyQt5.QtGui import QIcon
 from app import BrightnessControl
+
+def create_about_dialog():
+    msgBox = QMessageBox()
+    msgBox.setIcon(QMessageBox.Information)
+    msgBox.setText("This is a brightness control application.\n\n"
+        "For more information, please visit the project's GitHub page:\n"
+        "https://github.com/mvrck19/ddcbright\n\n"
+        "If you have any questions, you can contact me at:\n"
+        "phaidos@gmail.com\n\n"
+        "Icon made by Iconic Panda from Flaticon:\n"
+        "https://www.flaticon.com/free-icons/brightness")
+    msgBox.setWindowTitle("About DDCBright")
+    msgBox.setStandardButtons(QMessageBox.Ok)
+    msgBox.exec()
 
 app = QApplication([])
 app.setQuitOnLastWindowClosed(False)
@@ -19,6 +33,9 @@ tray.setVisible(True)
 
 # Create the menu
 menu = QMenu()
+about_action = QAction("About")
+about_action.triggered.connect(create_about_dialog)
+menu.addAction(about_action)
 quit_action = QAction("Quit")
 quit_action.triggered.connect(app.quit)
 menu.addAction(quit_action)
